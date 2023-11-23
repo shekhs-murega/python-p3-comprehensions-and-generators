b1cdcd462ad5d211dc0b22421fb8aaa707f8962f
shekinah <nyamburamary1212@gmail.com>
2023-11-23 09:14:41 +0300

    Complete comprehension and generators

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
-
+    even_list = [num for num in num_list if num % 2 == 0]
+    return even_list
 def make_exclamation(sentence_list):
-    pass
+    if not sentence_list:
+        return[]
+    
+    processed_list = [sentence + '!' for sentence in sentence_list]
+    return processed_list
